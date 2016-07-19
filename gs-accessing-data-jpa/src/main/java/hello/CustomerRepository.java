8e5260bdf76a2b1e2bbe1dfaf1edc62f0336f192
Matthew Diana <matthew.a.diana@gmail.com>
2016-07-19 16:23:04 -0400

    Finished gs-accessing-data-jpa

--- gs-accessing-data-jpa/src/main/java/hello/CustomerRepository.java
+++ gs-accessing-data-jpa/src/main/java/hello/CustomerRepository.java
@@ -2,7 +2,7 @@ package hello;
 
 import java.util.List;
 
-import org.springframework.data.repository.CredRepository;
+import org.springframework.data.repository.CrudRepository;
 
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
