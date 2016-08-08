dd82bbc8395eb092a55a142e2ec0bc00caab657f
Matthew Diana <matthew.a.diana@gmail.com>
2016-08-08 09:18:19 -0400

    Working on gs-messaging-redis

---
+++ gs-messaging-redis/src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+
