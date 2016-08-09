a1105b8eb158b18937f21ab4192fdd24f31ff780
Matthew Diana <matthew.a.diana@gmail.com>
2016-08-09 14:41:48 -0400

    Continuing gs-messaging-redis

--- gs-messaging-redis/src/main/java/hello/Application.java
+++ gs-messaging-redis/src/main/java/hello/Application.java
@@ -14,3 +14,23 @@ import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
+@SpringBootApplication
+public class Application {
+
+        private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+        @Bean
+        RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory,
+                        MessageListenerAdapter listenerAdapter) {
+
+            RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+            container.setConnectionFactory(connectionFactory);
+            container.addMessageListener(listenerAdapter, new PatternTopic("chat"));
+
+            return container;
+        }
+
+        @Bean
+        MessageListenerAdapter listenerAdapter(Receiver receiver) {
+            return new MessageListenerAdapter(receiver, "receiveMessage");
+        }
